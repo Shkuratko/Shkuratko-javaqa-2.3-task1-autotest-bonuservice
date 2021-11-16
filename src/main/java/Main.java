b5a3f0597df8ee0aa0b7e0c07ee3010aa4a8b997
Yuliya Shkuratko <sm0ci@mail.ru>
2021-11-16 23:45:11 +0300

    Delete target

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
-        BonusService service = new BonusService();
-
-        long amount = 1000_60;
-        boolean registered = true;
-        long expected = 30;
+public int number(int min, int max) {
+        int limit = 200;
+        for (int i = 0; i >= 10 && i <= 99; i++) {
+        for (int j = 0; j >= 10; j++) {
+        int multi = i * i;
+        if (multi > limit && multi < 300) {
 
         long actual = service.calculate(amount, registered);
 
